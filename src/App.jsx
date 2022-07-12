69b5a666629413acbfa08c2b523e80b199867d52
iZava <ignaciomzavalla@gmail.com>
2022-07-12 17:48:14 -0300

    modified a

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Footer from './components/Footer';
 function App() {
     return (
         <div className='bg-dark text-info'>
-           <a href="#" className='text-decoration-none'> <h1 className='text-center display-1 py-4'>Rick & Morty API </h1> </a>
+           <a href="https://izava.github.io/rick-and-morty-rest-api/" className='text-decoration-none'> <h1 className='text-center display-1 py-4'>Rick & Morty API </h1> </a>
             <CharacterList />
             <Footer />
 
