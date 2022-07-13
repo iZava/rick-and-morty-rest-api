bc4e15ecce83d29c656f00992a2f64f76b6b90b2
iZava <ignaciomzavalla@gmail.com>
2022-07-13 10:22:06 -0300

    config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://iZava.github.io/rick-and-morty-rest-api/",
+  base: "https://iZava.github.io/rick-and-morty-rest-api-react/",
   plugins: [react()]
 })
